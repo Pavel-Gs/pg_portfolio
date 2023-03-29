59480428bee93b4a526a82ae52e1cff9ad7f21d7
Pavel-Gs <pavelgen@gmail.com>
2023-03-29 15:30:13 -0700

    minor changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,29 @@
 // ABOUT FUNCTION
 export const About = () => {
 	return (<section id='about-section'>
-		<div><i className='fa-solid fa-file-lines' /><h3>INFO</h3></div>
-		<p>Hey, I'm Pavel - a geomatics engineer... Or rather I was, for 15 years. But a long story short - my goal now, is to change my career to Web Development.</p>
-		<p>Front-end technologies I'm working with are rather typical - JavaScript, CSS, HTML, React, SASS. Being a beginner in a new industry, I totally realize, how much I still have to learn...</p>
-		<p>Or should I, perhaps, rewrite the sentence and say: "I don't even realize yet the full depth of coding"?</p>
-		<p>But the learning process must never stop. So, if you wish, revisit this site in a few weeks - there might be a new project posted in the "projects" section.</p>
+		<div><i className='fa-solid fa-briefcase' /><h3>CAREER</h3></div>
+		<p>(Since 2018) Geomatics technologist. Self-employed.</p>
+		<p>(2014-2018) Survey assistant, party chief, drafter, technologist. Cadastral industry.</p>
+		<p>(2011-2013) Geomatics engineer. High-rise industry.</p>
+		<p>(2009-2011) Geomatics technician. Civil industry.</p>
+		<p>(2008-2009) Survey technician. Cadastral industry.</p>
 		<p className='spacer' />
 		<div><i className='fa-solid fa-graduation-cap' /><h3>EDUCATION</h3></div>
+		<p>Bachelor's degree in geomatics.</p>
+		<p><a href='https://cbeps-cceag.ca/' rel='noopener noreferrer' target='_blank'><i className='fa-solid fa-arrow-up-right-from-square' /> CBEPS</a></p>
+		<p><a href='https://www.bcit.ca/' rel='noopener noreferrer' target='_blank'><i className='fa-solid fa-arrow-up-right-from-square' /> BCIT</a></p>
+		<p><a href='https://www.miigaik.ru/en/' rel='noopener noreferrer' target='_blank'><i className='fa-solid fa-arrow-up-right-from-square' /> MIIGAiK</a></p>
+		<p className='spacer' />
+		<div><i className='fa-solid fa-file-lines' /><h3>CERTIFICATES</h3></div>
 		<p>
-			<a href='https://cbeps-cceag.ca/' rel='noopener noreferrer' target='_blank'><i className='fa-solid fa-arrow-up-right-from-square' /> CBEPS, </a>
-			<a href='https://www.bcit.ca/' rel='noopener noreferrer' target='_blank'><i className='fa-solid fa-arrow-up-right-from-square' /> BCIT, </a>
-			<a href='https://www.miigaik.ru/en/' rel='noopener noreferrer' target='_blank'><i className='fa-solid fa-arrow-up-right-from-square' /> MIIGAiK. </a>
-			Bachelor's degree in geomatics.
+			<a href='https://www.codecademy.com/profiles/pavelGeneralov3188521613/certificates/5f85dd867b67b60014ac9ea3' rel='noopener noreferrer' target='_blank'>
+				<i className='fa-solid fa-arrow-up-right-from-square' /> Front-end web development (Codecademy)
+			</a>
 		</p>
-		<p className='spacer' />
-		<div><i className='fa-solid fa-briefcase' /><h3>CAREER</h3></div>
-		<p>Geomatics engineer in land surveying, cadastral and civil industries (from 2008  - to 2023).</p>
-		<p className='spacer' />
-		<div><i className='fa-solid fa-graduation-cap' /><h3>CERTIFICATES</h3></div>
 		<p>
-			<a href='https://www.codecademy.com/profiles/pavelGeneralov3188521613/certificates/5f85dd867b67b60014ac9ea3' rel='noopener noreferrer' target='_blank'><i className='fa-solid fa-arrow-up-right-from-square' /> Front-end web development (Codecademy), </a>
-			<a href='https://drive.google.com/file/d/1eXcsDvLOU8kbuAzGn88OwOfkyE1ysV41/view?usp=sharing' rel='noopener noreferrer' target='_blank'><i className='fa-solid fa-arrow-up-right-from-square' /> Engineering and land surveying (CBEPS), </a>
+			<a href='https://drive.google.com/file/d/1eXcsDvLOU8kbuAzGn88OwOfkyE1ysV41/view?usp=sharing' rel='noopener noreferrer' target='_blank'>
+				<i className='fa-solid fa-arrow-up-right-from-square' /> Engineering and land surveying (CBEPS)
+			</a>
 		</p>
 		<p className='spacer' />
 	</section>);
--- src/components/Project4.js
+++ src/components/Project4.js
@@ -1,8 +1,8 @@
 // PROJECT4 FUNCTION
 export const Project4 = () => {
 	return (<section className='project-description-section'>
-		<div className='icon-div'><i className='fa-solid fa-file-lines' /><h3>MY RESUME</h3></div>
-		<p>The resume is somewhat similar to the "HTML-emails" project in terms of its structure. It is made with HTML tables and inline CSS styling. The reason for such simplicity is the possibility to copy-paste the page's content into MS Word and edit the contents further from there. Although, some styling will not be preserved, such as "font-size", "text-shadow", "box-shadow", "border-radius". But that can be compensated for in the text editor itself.</p>
+		<div className='icon-div'><i className='fa-solid fa-file-lines' /><h3>SKILL SHEET (WEB)</h3></div>
+		<p>The page is somewhat similar to the "HTML-emails" project in terms of its structure. It is made with HTML tables and inline CSS styling. The reason for such simplicity is the possibility to copy-paste the page's content into MS Word and edit the contents further from there. Although, some styling will not be preserved, such as "font-size", "text-shadow", "box-shadow", "border-radius". But that can be compensated for in the text editor itself.</p>
 		<p>The main advantage of this approach over "print as PDF" is in the preserved functionality and editability, while "print as PDF" saves the whole page as an image, making it uneditable.</p>
 		<p>How it works: open the resume web-page, "ctrl+a", "ctrl+c", "ctrl+v" into an empty MS Word page (set margins to 0). The text and images are selectable and could be moved, resized and styled. All the links are also preserved and are clickable.</p>
 		<p>For further usage, it is also possible to save the Word file as *.pdf, or paste the main HTML table into the email's body (paste as HTML, not just copy-paste).</p>
