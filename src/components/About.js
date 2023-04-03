587783176d3c761a7d719bc229099b4a39b09170
Pavel-Gs <pavelgen@gmail.com>
2023-04-03 12:23:21 -0700

    minor changes

--- src/components/About.js
+++ src/components/About.js
@@ -15,16 +15,8 @@ export const About = () => {
 		<p><a href='https://www.miigaik.ru/en/' rel='noopener noreferrer' target='_blank'><i className='fa-solid fa-arrow-up-right-from-square' /> MIIGAiK</a></p>
 		<p className='spacer' />
 		<div><i className='fa-solid fa-file-lines' /><h3>CERTIFICATES</h3></div>
-		<p>
-			<a href='https://www.codecademy.com/profiles/pavelGeneralov3188521613/certificates/5f85dd867b67b60014ac9ea3' rel='noopener noreferrer' target='_blank'>
-				<i className='fa-solid fa-arrow-up-right-from-square' /> Front-end web development (Codecademy)
-			</a>
-		</p>
-		<p>
-			<a href='https://drive.google.com/file/d/1eXcsDvLOU8kbuAzGn88OwOfkyE1ysV41/view?usp=sharing' rel='noopener noreferrer' target='_blank'>
-				<i className='fa-solid fa-arrow-up-right-from-square' /> Engineering and land surveying (CBEPS)
-			</a>
-		</p>
+		<p><i className='fa-solid fa-arrow-up-right-from-square' /> Front-end web development (Codecademy)</p>
+		<p><i className='fa-solid fa-arrow-up-right-from-square' /> Engineering and land surveying (CBEPS)</p>
 		<p className='spacer' />
 	</section>);
 };
