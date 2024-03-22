a65e8adee8963d4b8e05bf56e89b8eddf2101fb8
Pavel-Gs <pavelgen@gmail.com>
2024-03-22 14:30:31 -0700

    added links and description for Project Tracker

--- src/components/Project3.js
+++ src/components/Project3.js
@@ -2,7 +2,7 @@
 export const Project3 = () => {
 	return (<section className='project-description-section'>
 		<div className='icon-div'><i className='fa-solid fa-file-lines' /><h3>PROJECT TRACKER</h3></div>
-		<p>This application combines the functionalities of a job tracker, time-sheets, and project database, providing a comprehensive solution for efficient project management. With its user-friendly interface and robust features, it streamlines workflow, enhances collaboration, and ensures accurate tracking of tasks and resources.</p>
+		<p>This application combines the functionalities of a job tracker, map, time-sheets, and project database, providing a comprehensive solution for efficient project management. With its user-friendly interface and robust features, it streamlines workflow, enhances collaboration, and ensures accurate tracking of tasks and resources.</p>
 		<p className='spacer' />
 		<div className='icon-div'><i className='fa-solid fa-toolbox' /><h3>TECHNOLOGIES</h3></div>
 		<div className='description-div'><i className='fa-solid fa-n' /><p>NextJS: allows to create full-stack web applications with the power of React components.</p></div>
@@ -12,8 +12,10 @@ export const Project3 = () => {
 		<div className='icon-div'><i className='fa-solid fa-link' /><h3>INTEGRATIONS</h3></div>
 		<div className='description-div'><i className='fa-solid fa-square-share-nodes' /><p>Clerk: this is a complete suite of embeddable UIs, flexible APIs, and admin dashboards to authenticate and manage users."</p></div>
 		<div className='description-div'><i className='fa-solid fa-square-share-nodes' /><p>Shadcn-UI: pre-designed components that can be copied and pasted into apps; accessible, customizable and open source.</p></div>
+		<div className='description-div'><i className='fa-solid fa-square-share-nodes' /><p>Leaflet: this is an open source JavaScript library used to build web mapping applications.</p></div>
 		<p className='spacer' />
 		<div className='icon-div'><h3>LINKS</h3></div>
+		<div className='description-div'><i className='fa-solid fa-globe' /><p><a href='https://job-tracker-chi.vercel.app/' rel='noopener noreferrer' target='_blank'>Published site</a></p></div>
 		<div className='description-div'><i className='fa-brands fa-github' /><p><a href='https://github.com/Pavel-Gs' rel='noopener noreferrer' target='_blank'>Git repo (this repo is not in public access)</a></p></div>
 		<p className='spacer' />
 	</section>);
