75a5351f330bff20184850d430d0edac5e28775a
Pavel-Gs <pavelgen@gmail.com>
2024-11-14 15:23:17 -0800

    changed cards order

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -1,22 +1,22 @@
-// PROJECT1 FUNCTION
+// PROJECT3 FUNCTION
 export const Project1 = () => {
 	return (<section className='project-description-section'>
-		<div className='icon-div'><i className='fa-solid fa-file-lines' /><h3>ONLINE HTML-EMAILS</h3></div>
-		<p>Send HTML-styled emails from any device with this online app. It is pre-built for quotes, proposals, work orders, etc...</p>
+		<div className='icon-div'><i className='fa-solid fa-file-lines' /><h3>PROJECT TRACKER</h3></div>
+		<p>This application combines the functionalities of a job tracker, project map, time-sheets, and SQL project database, providing a comprehensive solution for efficient project management. With its user-friendly interface and robust features, it streamlines workflow, enhances collaboration, and ensures accurate tracking of tasks and resources. (Inspired by John Smilga's "Jobify")</p>
 		<p className='spacer' />
 		<div className='icon-div'><i className='fa-solid fa-toolbox' /><h3>TECHNOLOGIES</h3></div>
-		<div className='description-div'><i className='fa-brands fa-js' /><p>JavaScript: vanilla JavaScript in this project is used primarily for hiding/showing elements, changing CSS styles and iterating through components.</p></div>
-		<div className='description-div'><i className='fa-brands fa-css3-alt' /><p>CSS: regular CSS is used for the web view. The In-line CSS is used for the email-client view. Note: different email clients interpret the CSS differently.</p></div>
-		<div className='description-div'><i className='fa-solid fa-file-code' /><p>HTML: HTML emails are usually developed using tables (a lot of tables) and archaic-looking in-line CSS styling.</p></div>
+		<div className='description-div'><i className='fa-solid fa-n' /><p>NextJS: allows to create full-stack web applications with the power of React components.</p></div>
+		<div className='description-div'><i className='fa-solid fa-t' /><p>TypeScript: adds additional syntax to JavaScript and uses type inference, which provides a great tooling without additional code.</p></div>
+		<div className='description-div'><i className='fa-regular fa-file-code' /><p>Tailwind CSS: a utility-first CSS framework, that can be composed to build any design, directly in the markup.</p></div>
 		<p className='spacer' />
 		<div className='icon-div'><i className='fa-solid fa-link' /><h3>INTEGRATIONS</h3></div>
-		<div className='description-div'><i className='fa-solid fa-square-share-nodes' /><p>SmtpJS: this integration allows you to grab the inner HTML, put it into the email's body and send it within a JavaScript environment.</p></div>
-		<div className='description-div'><i className='fa-solid fa-square-share-nodes' /><p>Elastic Email: this integration provides email tracking and marketing metrics, such as clicks, time, location, device, etc...</p></div>
-		<div className='description-div'><i className='fa-solid fa-square-share-nodes' /><p>FormSubmit: as the name suggests, that integration provides easy form-submission functionality and automated replies.</p></div>
+		<div className='description-div'><i className='fa-solid fa-square-share-nodes' /><p>Clerk: this is a complete suite of embeddable UIs, flexible APIs, and admin dashboards to authenticate and manage users."</p></div>
+		<div className='description-div'><i className='fa-solid fa-square-share-nodes' /><p>Shadcn-UI: pre-designed components that can be copied and pasted into apps; accessible, customizable and open source.</p></div>
+		<div className='description-div'><i className='fa-solid fa-square-share-nodes' /><p>Leaflet: this is an open source JavaScript library used to build web mapping applications.</p></div>
 		<p className='spacer' />
 		<div className='icon-div'><h3>LINKS</h3></div>
-		<div className='description-div'><i className='fa-solid fa-globe' /><p><a href='https://html-quotes.netlify.app/' rel='noopener noreferrer' target='_blank'>Published site</a></p></div>
-		<div className='description-div'><i className='fa-brands fa-github' /><p><a href='https://github.com/Pavel-Gs/html_emails' rel='noopener noreferrer' target='_blank'>Git repo</a></p></div>
+		<div className='description-div'><i className='fa-solid fa-globe' /><p><a href='https://job-tracker-pg.vercel.app/' rel='noopener noreferrer' target='_blank'>Published site</a></p></div>
+		<div className='description-div'><i className='fa-brands fa-github' /><p><a href='https://github.com/Pavel-Gs' rel='noopener noreferrer' target='_blank'>Git repo (this repo is not in public access)</a></p></div>
 		<p className='spacer' />
 	</section>);
 };
