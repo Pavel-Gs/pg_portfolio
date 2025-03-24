0822131753cbd7e13f3b738d6c927714d781f808
Pavel-Gs <pavelgen@gmail.com>
2025-03-24 15:54:21 -0700

    Updated Git link

--- src/components/Project4.js
+++ src/components/Project4.js
@@ -13,7 +13,7 @@ export const Project4 = () => {
 		<p className='spacer' />
 		<div className='icon-div'><h3>LINKS</h3></div>
 		<div className='description-div'><i className='fa-solid fa-globe' /><p><a href='https://familyspace-rentals.netlify.app/' rel='noopener noreferrer' target='_blank'>Published site</a></p></div>
-		<div className='description-div'><i className='fa-brands fa-github' /><p><a href='https://github.com/Pavel-Gs' rel='noopener noreferrer' target='_blank'>Git repo (this repo is not in public access)</a></p></div>
+		<div className='description-div'><i className='fa-brands fa-github' /><p><a href='https://github.com/Pavel-Gs/familyspace' rel='noopener noreferrer' target='_blank'>Git repo</a></p></div>
 		<p className='spacer' />
 	</section>);
 };
